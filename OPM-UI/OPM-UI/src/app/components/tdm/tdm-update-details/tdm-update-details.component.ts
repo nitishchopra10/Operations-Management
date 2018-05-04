33ce0629dc5444c034792ed220f2323bdff3b946
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2018-05-04 17:10:59 +0530

    friday update
    tdm Update module created, tdm add employee module
    
    corrected configurations for liquibase and schema of tdm service

--- OPM-UI/OPM-UI/src/app/components/tdm/tdm-update-details/tdm-update-details.component.ts
+++ OPM-UI/OPM-UI/src/app/components/tdm/tdm-update-details/tdm-update-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-tdm-update-details',
@@ -8,10 +9,13 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class TdmUpdateDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  employeeData;
   updateTeamMemberForm;
   ngOnInit() {
+
+    this.setTable();
     this.updateTeamMemberForm = new FormGroup({
       employeeId: new FormControl(),
       employeeName: new FormControl(),
@@ -26,19 +30,51 @@ export class TdmUpdateDetailsComponent implements OnInit {
     });
   }
 
+  setTable() {
+    this.http.get("http://localhost:9000/tdm/all").map(res => res.json()).subscribe(data => {
+
+      this.employeeData = data;
+      console.log(data);
+    })
+
+  }
+
   onSubmit(data) {
-    let  employee={
+    let employee = {
       "empId": data.employeeId,
-       "name": data.employeeName,
-       "mCode": data.mCode,
-       "contactNumber": data.contactNumber,
-       "project": data.project,
-       "address": data.address,
-       "subLevel": data.subLevel,
-       "n1": data.n1,
-       "n2" : data.n2
+      "name": data.employeeName,
+      "mCode": data.mCode,
+      "contactNumber": data.contactNumber,
+      "project": data.project,
+      "address": data.address,
+      "subLevel": data.subLevel,
+      "n1": data.n1,
+      "n2": data.n2
     }
     console.log(employee);
 
+    this.http.post("http://localhost:9000/tdm/update", employee).subscribe(res => {
+      alert(res.status + "  " + res.statusText);
+      this.setTable();
+      this.updateTeamMemberForm.reset();
+    })
+
   }
+
+  setData(data) {
+
+    this.updateTeamMemberForm.controls['employeeId'].setValue(data.empId);
+    this.updateTeamMemberForm.controls['employeeName'].setValue(data.name);
+    this.updateTeamMemberForm.controls['mCode'].setValue(data.mCode);
+    this.updateTeamMemberForm.controls['subLevel'].setValue(data.subLevel);
+    this.updateTeamMemberForm.controls['project'].setValue(data.project);
+    this.updateTeamMemberForm.controls['n1'].setValue(data.n1);
+    this.updateTeamMemberForm.controls['n2'].setValue(data.n2);
+    this.updateTeamMemberForm.controls['contactNumber'].setValue(data.contactNumber);
+    this.updateTeamMemberForm.controls['address'].setValue(data.address);
+
+    console.log("hello")
+
+  }
+
 }
