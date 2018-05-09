e7ffda755a5f3c191ee0bde878bc77243a3627ef
nitishchopra10 <nitishchopra10@gmail.com>
2018-05-09 14:58:36 +0530

    adding delivery portfolio
    delivery portfolio service

---
+++ MicroServices/delivery_portfolio/src/main/java/com/delivery/portfolio/DeliveryPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class DeliveryPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryPortfolioApplication.class, args);
+	}
+}
